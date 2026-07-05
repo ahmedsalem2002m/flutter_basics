0ee027d588bf7e9fb066ca9d6881fa9a72910256
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-07-05 17:04:32 +0300

    Scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Center(
-          child: Text(
-              "Hello World"
-          ),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello World"),
+        ),
       ),
     ),
   );
