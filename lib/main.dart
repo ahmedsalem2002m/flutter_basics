5bfaf8fe8b746db6a4235124154e35b83266bb48
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-07-05 17:46:02 +0300

    custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+      MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text("Hello"),
+        ),
         body: Center(
-          child: Text("Hello World"),
+          child: Text("Hello world"),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
