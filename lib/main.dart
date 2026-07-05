177244aaa9dc902e34001c2cb17bfcb97cff2a9a
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-07-05 18:05:42 +0300

    Column widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,22 +15,50 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         floatingActionButton: FloatingActionButton(onPressed: () {}),
         appBar: AppBar(
-          backgroundColor: Colors.black,
+          backgroundColor: Colors.blueGrey,
           title: Text("Hello"),
         ),
-        body: Center(
-          child: Container(
-            height: 100,
-            width: 200,
-            color: Colors.green,
-            child: Center(
-              child: Text("Hello world",
-                style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.black87
-                ),),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.green,
+              child: Center(
+                child: Text("Hello world",
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black87
+                  ),),
+              ),
             ),
-          ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.red,
+              child: Center(
+                child: Text("Hello world",
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black87
+                  ),),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 300,
+              color: Colors.deepPurple,
+              child: Center(
+                child: Text("Hello world",
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black87
+                  ),),
+              ),
+            ),
+          ],
         ),
       ),
     );
